cd8303b0447042af2e87e9f364a92e665291942b
caferrerb <caferrerb@eam.edu.co>
2016-10-04 08:57:26 -0500

    modificacion del metodo

--- biblioteca/biblioteca-logica/src/main/java/co/edu/eam/biblioteca/logica/bos/LibroBO.java
+++ biblioteca/biblioteca-logica/src/main/java/co/edu/eam/biblioteca/logica/bos/LibroBO.java
@@ -1,11 +1,13 @@
 package co.edu.eam.biblioteca.logica.bos;
 
+import co.edu.eam.biblioteca.persistencia.modelo.entidades.Libro;
+
 public class LibroBO {
 	public LibroBO() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void crear(){
+	public void crear(Libro libro){
 		
 	}
 
